09205fc4a593118fc391576af1e3f48284851b02
mhaekals <mohammed_haekal@yahoo.com>
2021-02-05 17:14:56 +0700

    commit backend

---
+++ routes/products.js
@@ -0,0 +1,194 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../models/product')
+const Category  = require('../models/category')
+const mongoose = require('mongoose')
+const multer = require('multer')
+
+
+// to upload files
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if(isValid) {
+            uploadError = null
+        }
+      cb(uploadError, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        
+      const fileName = file.originalname.split(' ').join('-');
+      const extension = FILE_TYPE_MAP[file.mimetype];
+      cb(null, `${fileName}-${Date.now()}.${extension}`)
+    }
+  })
+  
+const uploadOptions = multer({ storage: storage })
+
+// read/get all products
+router.get(`/`, async (req, res) =>{
+    let filter = {};
+    if(req.query.categories)
+    {
+         filter = {category: req.query.categories.split(',')} //to get multiple categories
+    }
+
+    const productList = await Product.find(filter).populate('category');
+
+    if(!productList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(productList);
+})
+
+
+// read/get products by id
+router.get(`/:id`, async (req, res) =>{
+    const product = await Product.findById(req.params.id).populate('category'); // to get category data -> the category comes from the product model
+
+    if(!product) {
+        res.status(500).json({success: false})
+    } 
+    res.send(product);
+})
+
+// create/post new product
+router.post(`/`, uploadOptions.single('image'), async (req, res) =>{
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category') // category must be valid else will not be posted
+
+    const file = req.file;
+    if(!file) return res.status(400).send('No image in the request')
+
+    const fileName = file.filename
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: `${basePath}${fileName}`,// "http://localhost:3000/public/upload/image-2323232"
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+
+    product = await product.save();
+
+    if(!product) 
+    return res.status(500).send('The product cannot be created')
+
+    res.send(product);
+})
+
+// update/put products by id
+router.put('/:id',async (req, res)=> {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+       return res.status(400).send('Invalid Product Id') // to validate the product id
+    }
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category') // category must be valid else will not be edited
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        { new: true}
+    )
+
+    if(!product)
+    return res.status(500).send('the product cannot be updated!')
+
+    res.send(product);
+})
+
+// delete products by id
+router.delete('/:id', (req, res)=>{
+    Product.findByIdAndRemove(req.params.id).then(product =>{
+        if(product) {
+            return res.status(200).json({success: true, message: 'the product is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "product not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+// to show how many product do we have in database (for admin)
+router.get(`/get/count`, async (req, res) =>{
+    const productCount = await Product.countDocuments((count) => count)
+
+    if(!productCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        productCount: productCount
+    });
+})
+
+// to show featured product 
+router.get(`/get/featured/:count`, async (req, res) =>{
+    const count = req.params.count ? req.params.count : 0
+    const products = await Product.find({isFeatured: true}).limit(+count); //+count is to convert string to number
+
+    if(!products) {
+        res.status(500).json({success: false})
+    } 
+    res.send(products);
+})
+
+// to edit product & adding multiple images
+//to add multiple images with max 10 images in one request -> line 165
+router.put('/gallery-images/:id', uploadOptions.array('images', 10), async (req, res)=> {
+        if(!mongoose.isValidObjectId(req.params.id)) {
+            return res.status(400).send('Invalid Product Id')
+         }
+         const files = req.files
+         let imagesPaths = [];
+         const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
+         if(files) {
+            files.map(file =>{
+                imagesPaths.push(`${basePath}${file.filename}`);
+            })
+         }
+
+         const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                images: imagesPaths
+            },
+            { new: true}
+        )
+
+        if(!product)
+            return res.status(500).send('the gallery cannot be updated!')
+
+        res.send(product);
+    }
+)
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,159 @@
+const User = require('../models/user')
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config
+
+
+// read/get all users
+router.get(`/`, async (req, res) =>{
+    const userList = await User.find().select('-passwordHash') // to exclude password when call the users api
+
+    if(!userList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(userList);
+})
+
+// read/get users by id
+router.get('/:id', async(req,res)=>{
+    const user = await User.findById(req.params.id).select('-passwordHash'); // to exclude password when call the users api
+
+    if(!user) {
+        res.status(500).json({message: 'The user with the given ID was not found.'})
+    } 
+    res.status(200).send(user);
+})
+
+
+// create/post new user
+router.post('/', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+// update/put users by id
+router.put('/:id',async (req, res)=> {
+
+    const userExist = await User.findById(req.params.id);
+    let newPassword
+    if(req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10)
+    } else {
+        newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+
+// to login
+router.post('/login', async (req,res) => {
+    const user = await User.findOne({email: req.body.email})
+    const SECRET_TOKEN = process.env.SECRET_TOKEN;
+    if(!user) {
+        return res.status(400).send('The user not found');
+    }
+
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                isAdmin: user.isAdmin
+            },
+            SECRET_TOKEN,
+            {expiresIn : '1d'} // to specify when the token is expired
+        )
+       
+        res.status(200).send({user: user.email , token: token}) 
+    } else {
+       res.status(400).send('password is wrong!');
+    }})
+
+
+// to register
+router.post('/register', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+//to delete users by id
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if(user) {
+            return res.status(200).json({success: true, message: 'the user is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "user not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+// to show how many customers does that admin has
+router.get(`/get/count`, async (req, res) =>{
+    const userCount = await User.countDocuments((count) => count)
+
+    if(!userCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        userCount: userCount
+    });
+})
+    
+
+module.exports = router;
